3f228e1ad26f426ef65699565e5ae39c8a2bcc2e
JoanBarFul <91464434+JoanBarFul@users.noreply.github.com>
2025-01-14 17:29:37 +0100

    splash

---
+++ app/src/main/java/com/example/splash/MainActivity2.java
@@ -0,0 +1,70 @@
+package com.example.splash;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        //Fade1
+        TextView logo1 = findViewById(R.id.TextViewTopTitle);
+        Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo1.startAnimation(fade1);
+
+        //Fade2
+        TextView logo2 = findViewById(R.id.TextViewBottomTitle);
+        Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
+        logo2.startAnimation(fade2);
+
+        // Fade2 list5ener since it's the longer one
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(MainActivity2.this, MainActivity2.class));
+                finish(); // Important to finish so you cant go back with the arrow
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+        //Table
+        Animation spin = AnimationUtils.loadAnimation(this, R.anim.animation);
+        LayoutAnimationController controller = new LayoutAnimationController(spin);
+        TableLayout table = findViewById(R.id.TableLayout01);
+        for (int i = 0; i < table.getChildCount(); i++) {
+            TableRow row = (TableRow) table.getChildAt(i);
+            row.setLayoutAnimation(controller);
+        }
+    }
+}
